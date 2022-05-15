1858f6b86e1df874ab0b9aa073abedf81b993de1
zackmolt <fkrihkl@gmail.com>
2022-05-15 15:58:23 +0700

    add order confirmation and order detail page

--- Html/order_list.js
+++ Html/order_list.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
     // Check wether the user is already logged in or not
-    checkSession();
+    getRecipe();
 
 });
 
-async function checkSession(){
+async function getRecipe(){
     let token = localStorage.getItem('token');
     $.ajax({
-        url: 'https://api.fikrihkl.xyz/api/secret-route',
+        url: 'https://api.fikrihkl.xyz/api/order/get-list',
         type: 'GET',
         dataType: 'json',
         headers: {
@@ -17,9 +17,21 @@ async function checkSession(){
         },
         contentType: 'application/json; charset=utf-8',
         success: function (result) {
-            if(result.status === false){
-                window.location.replace("Login.html");
-            }
+            console.log(result);
+            let listOrder = result.data;
+            let listItemHtml = [];
+            listOrder.forEach(e => {
+                let element = `<div>${e.address}</div>`;
+                element += `<div>${e.phone}</div>`;
+                element += `<div>${e.total_price}</div>`;
+                element += `<div>${e.delivery_fee}</div>`;
+                element += `<div>${e.status}</div>`;
+                element += `<div>${e.name}</div>`;
+                element += `<img src="${e.img_url}"/>`;
+
+                listItemHtml.push(element);
+            });
+            $('#list-order').append(listItemHtml);
         },
         error: function (error) {
             window.location.replace("Login.html");
