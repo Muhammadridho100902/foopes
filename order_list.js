47deb9521433a898895dc87a6317f2b434fac46e
unknown <uwater946@gmail.com>
2022-05-16 13:56:25 +0700

    add order detail

--- order_list.js
+++ order_list.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     // Check wether the user is already logged in or not
     getRecipe();
 
 });
 
-async function getRecipe(){
+async function getRecipe() {
     let token = localStorage.getItem('token');
     $.ajax({
         url: 'https://api.fikrihkl.xyz/api/order/get-list',
@@ -16,24 +16,35 @@ async function getRecipe(){
             'Accept': 'appplication/json'
         },
         contentType: 'application/json; charset=utf-8',
-        success: function (result) {
+        success: function(result) {
             console.log(result);
             let listOrder = result.data;
             let listItemHtml = [];
             listOrder.forEach(e => {
-                let element = `<div>${e.address}</div>`;
-                element += `<div>${e.phone}</div>`;
-                element += `<div>${e.total_price}</div>`;
-                element += `<div>${e.delivery_fee}</div>`;
-                element += `<div>${e.status}</div>`;
-                element += `<div>${e.name}</div>`;
-                element += `<img src="${e.img_url}"/>`;
+                let element = `<div class="card">
+                <div class="card-body">
+                    <div class="card p-md-4 shadow-lg mt-2">
+                        <h6>Shopping ${e.order_date} <span class="status-order">${e.status}</span></h6>
+                        <h6>Foopes Web Order List</h6>
+                        <div class="details pt-1">
+                            <div class="container-img">
+                                <img class="container-img" src="${e.img_url}"></img>
+                            </div>
+                            <div class="txt-next">
+                                <h6>${e.recipe_name}</h6>
+                                <h6 style="font-weight: lighter">Delivery Fee: Rp. ${e.delivery_fee}</h6>
+                                <h6 class="pt-4" style="font-weight: 500">Total Price: Rp. ${e.total_price}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 
                 listItemHtml.push(element);
             });
             $('#list-order').append(listItemHtml);
         },
-        error: function (error) {
+        error: function(error) {
             window.location.replace("Login.html");
         }
     });
